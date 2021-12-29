19784f4e3380c89a60c14dde2c415d17b62fe873
yarentamir <yarden.tamir@walla.co.il>
2021-12-29 22:07:01 +0200

    last try

--- src/Pages/TeddyBearGame.jsx
+++ src/Pages/TeddyBearGame.jsx
@@ -3,11 +3,12 @@ import '../styles/StyleSheet1.css';
 
 export default function TeddyGamePage() {
   return (
+
     <div id="DivGame">
       <iframe
         id="game"
         alt="fds"
-        src="../gameFiles/TeddyBearMachine_YardenTamir_AmitMiller_Jso.html"
+        src="TeddyBearMachine_YardenTamir_AmitMiller_Jso.html"
         title="he"
       ></iframe>
     </div>
