e6091656ed8d1b29b783cbf675c87257e62f9751
yarentamir <yarden.tamir@walla.co.il>
2022-01-01 17:36:33 +0200

    start

--- src/App.js
+++ src/App.js
@@ -1,103 +1,39 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useMemo } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Header from "./components/Header";
-import Game from "./Pages/TeddyBearGame";
-import HomePage from "./Pages/HomePage";
-import "./styles/myStyle.css";
-import { UserContext } from "./context/userContext";
-import InitialState from "./Utilities/initialState";
+import Header from "./Components/Header";
+import Home from "./Pages/Home";
+import Login from "./Pages/Login";
+// import "./styles/myStyle.css";
+import { UserContext, RandomRecipes } from "./Context/userContext";
+import { getRandomRecipes } from "./Utilities/api";
+import { db } from "./Utilities/firebase-config";
+import { collection, getDocs } from "firebase/firestore";
 
 function App() {
-  const [value, setValue] = useState(InitialState);
+  const [user, setUser] = useState([]);
+  const [dataState, setData] = useState([]);
+  const usersCollectionRef = collection(db, "users");
+  const value = useMemo(() => ({ user, setUser }), [user, setUser]);
+  const data = useMemo(() => ({ dataState, setData }), [dataState, setData]);
 
-  // componentDidMount = () => {
-  //   this.start();
-  // };
-
-  // start = async () => {
-  //   const data = await Api.getItems();
-  //   this.setState({
-  //     data,
-  //     dataToFilter: data,
-  //     randomQuestionIndex: Math.floor(Math.random() * data.length),
-  //     countRights: data.filter((item) => item.wasRight).length,
-  //   });
-  //   console.log(this.state);
-  // };
-
-  // handelNewCardClick = () => {
-  //   this.setState({
-  //     randomQuestionIndex: Math.floor(
-  //       Math.random() * this.state.dataToFilter.length
-  //     ),
-  //     showQuestionOrAnswer: "question",
-  //   });
-  // };
-
-  // handelRevealAnswerClick = () => {
-  //   this.setState({
-  //     showQuestionOrAnswer: "answer",
-  //   });
-  // };
-
-  // handelRightClick = (item) => {
-  //   const dataToFilter = this.state.dataToFilter.filter(
-  //     (itemData) => itemData.id !== item.id
-  //   );
-  //   this.setState((prevState) => ({
-  //     countRights: prevState.countRights + 1,
-  //     dataToFilter,
-  //     randomQuestionIndex: Math.floor(Math.random() * dataToFilter.length),
-  //     showQuestionOrAnswer: "question",
-  //   }));
-  // };
-
-  // handelReshuffleClick = async () => {
-  //   this.setState(InitialState);
-  //   this.start();
-  // };
-
-  // handleClickEditCard = (item) => {
-  //   this.setState({ currentEdit: item });
-  // };
-
-  // handleChangeAdd = async (event) => {
-  //   const { name, value, id } = event.target;
-  //   this.setState((prevState) => ({
-  //     [id]: { ...prevState[id], [name]: value },
-  //   }));
-  // };
-
-  // handleSubmitAdd = async () => {
-  //   await Api.addItem(this.state.addQuestion);
-  //   this.handelReshuffleClick();
-  //   this.setState({ addQuestion: {} });
-  // };
-
-  // handleClickDelete = async (Item) => {
-  //   await Api.deleteItem(Item.id);
-  //   await this.updateData();
-  // };
-
-  // handleClickEdit = async (item) => {
-  //   await Api.editItem(item.id, this.state.currentEdit);
-  //   await this.updateData();
-  // };
-
-  // updateData = async () => {
-  //   const data = await Api.getItems();
-  //   this.setState({ data });
-  // };
+  useEffect(() => {
+    const getUsers = async () => {
+      const data = await getDocs(usersCollectionRef);
+      setUser(data.docs.map((doc) => ({ ...doc.data(), id: doc.id })));
+      setData(await getRandomRecipes());
+    };
+    getUsers();
+  }, []);
 
   return (
     <BrowserRouter>
       <Header />
-      <UserContext.Provider value={{ value, setValue }}>
+      <RandomRecipes.Provider data={{ data, setData }}>
         <Routes>
-          <Route path="/" exact element={<HomePage />} />
-          <Route path="/game" exact element={<Game />} />
+          <Route path="/" exact element={<Login />} />
+          <Route path="/game" exact element={<Home />} />
         </Routes>
-      </UserContext.Provider>
+      </RandomRecipes.Provider>
     </BrowserRouter>
   );
 }
